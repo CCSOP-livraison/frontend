ea8e78f32e1f821e219d8c9dbfc211333e448751
Elodie-Depierraz <elodie.depierraz@eduvaud.ch>
2026-08-31 10:49:20 +0200

    feat: add header and footer

--- CCSOP/src/App.vue
+++ CCSOP/src/App.vue
@@ -1,3 +1,9 @@
+<script setup>
+import AppFooter from './components/footer.vue'
+import AppHeader from './components/header.vue'
+</script>
 <template>
+  <AppHeader />
   <RouterView />
+  <AppFooter />
 </template>
---
+++ CCSOP/src/components/footer.vue
@@ -0,0 +1,110 @@
+<script setup></script>
+
+<template>
+  <footer
+    class="u-align-center u-clearfix u-container-align-center u-footer u-grey-80 u-footer"
+    id="footer"
+  >
+    <div class="u-clearfix u-sheet u-sheet-1">
+      <nav class="u-menu u-menu-one-level u-offcanvas u-menu-1" role="navigation">
+        <div class="menu-collapse">
+          <a
+            class="u-button-style u-hamburger-link u-nav-link"
+            aria-label="Ouvrir le menu"
+            aria-controls="78af"
+            href="#"
+          >
+            <svg class="u-svg-link" viewBox="0 0 24 24"><use xlink:href="#svg-ad78"></use></svg>
+            <svg
+              class="u-svg-content"
+              version="1.1"
+              id="svg-ad78"
+              viewBox="0 0 16 16"
+              x="0px"
+              y="0px"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g>
+                <rect y="1" width="16" height="2"></rect>
+                <rect y="7" width="16" height="2"></rect>
+                <rect y="13" width="16" height="2"></rect>
+              </g>
+            </svg>
+          </a>
+        </div>
+        <div class="u-nav-container"></div>
+        <div id="78af" role="region" aria-label="Panneau du menu" class="u-nav-container-collapse">
+          <div
+            class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav"
+          >
+            <div class="u-inner-container-layout u-sidenav-overflow">
+              <div
+                class="u-menu-close"
+                tabindex="-1"
+                aria-controls="78af"
+                aria-label="Fermer le menu"
+              ></div>
+              <ul role="menubar" class="u-align-left u-nav u-popupmenu-items u-unstyled u-nav-1">
+                <li role="none" class="u-nav-item">
+                  <a role="menuitem" class="u-button-style u-nav-link" href="./"
+                    >register standard</a
+                  >
+                </li>
+                <li role="none" class="u-nav-item">
+                  <a
+                    role="menuitem"
+                    class="u-button-style u-nav-link"
+                    href="register-restaurateur.html"
+                    >register restaurateur</a
+                  >
+                </li>
+                <li role="none" class="u-nav-item">
+                  <a role="menuitem" class="u-button-style u-nav-link" href="login.html">login</a>
+                </li>
+                <li role="none" class="u-nav-item">
+                  <a role="menuitem" class="u-button-style u-nav-link" href="Page-1.html">Page 1</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+        </div>
+      </nav>
+      <div class="u-list u-list-1">
+        <div class="u-repeater u-repeater-1">
+          <div class="u-container-style u-list-item u-repeater-item">
+            <div class="u-container-layout u-similar-container u-container-layout-1">
+              <p class="u-text u-text-1">contact :</p>
+              <ul class="u-text u-text-2">
+                <li>tel : +41 12 345 67 89</li>
+                <li>mail : test@domaine.ch</li>
+              </ul>
+            </div>
+          </div>
+          <div class="u-container-style u-list-item u-repeater-item">
+            <div class="u-container-layout u-similar-container u-container-layout-2">
+              <p class="u-text u-text-3">liens autre</p>
+              <ul class="u-text u-text-4">
+                <li>accueil</li>
+                <li>se déconnecter</li>
+                <li>test1234</li>
+              </ul>
+            </div>
+          </div>
+          <div class="u-container-style u-list-item u-repeater-item">
+            <div class="u-container-layout u-similar-container u-container-layout-3">
+              <p class="u-text u-text-5">information légal :&nbsp;</p>
+              <ul class="u-text u-text-6">
+                <li><span class="u-text-palette-1-base">Charte d'utilisation des données</span></li>
+                <li><span class="u-text-palette-1-base">condition générale</span></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style scoped></style>
